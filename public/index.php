3092d12670713c89b69d99165511104fad6333ab
Kaan Tolunay Kilic <kaan-tolunay@hotmail.de>
2018-10-03 11:54:40 +0200

    content: basic set up and dependencies installed

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__.'/../src/Kernel.php';
+
+
+$environment = $_SERVER['APP_ENV'] ?? 'dev';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
+
+$kernel = new Kernel($environment, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
